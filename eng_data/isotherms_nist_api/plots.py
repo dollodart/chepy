74395dcc150691add6421249cf881f196fc32212
david <davidollodart@gmail.com>
2021-11-14 14:06:11 -0800

    refactor plots -> plots & tables

--- eng_data/isotherms_nist_api/plots.py
+++ eng_data/isotherms_nist_api/plots.py
@@ -1,5 +1,4 @@
 from fits import fit_langmuir_explicit, fit_langmuirs, fit_freundlichs
-from read_data import read_isotherm_data
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -77,65 +76,10 @@ def plot_all_isotherms(df):
         plt.xlabel('pressure in ' + gr['pressUnit'].iloc[0])
         plt.ylabel('adsorption in ' + gr['adsUnit'].iloc[0])
 
-def p05(x):
-    return np.quantile(x, 0.05)
-def p50(x):
-    return np.quantile(x, 0.50)
-def p95(x):
-    return np.quantile(x, 0.95)
-
-def table_ind_means(df):
-    g = df.groupby('adsorbate')['temperature', 'pressure']
-    res = g.agg([np.mean, np.std, p05, p95])
-    res = res.sort_values(by=[('temperature', 'mean'), ('pressure', 'mean')])
-    return res
-
-def table_ind_ranges(df):
-    def maxminmin(x):
-        return p95(x) - p05(x)
-
-    def maxminminnorm(x):
-        return maxminmin(x) / p50(x)
-
-    for func, st in (maxminmin, 'max-min'), (maxminminnorm, '(max-min)/med'):
-        g = df.groupby(['doi', 'adsorbate', 'adsorbent'])
-        gg = g['pressure'].agg(func)
-        print('pressure mean, max, min of', st, gg.mean(), gg.max(), gg.min(), sep='\n')
-        gg = g['temperature'].agg(func)
-        bl = gg > 0
-        print('temperature (excluding 0) mean, max, min of', st, gg[bl].mean(), gg[bl].max(), gg[bl].min(), sep='\n')
-        print('excluded', bl.sum() / len(bl), 'of datapoints')
-    return None
-
-def test_invar(df):
-    df['invar'] = np.log(df['pressure']) * df['temperature'] * np.log(df['temperature'])
-    invars = []
-    for n, gr in df.drop_duplicates(['pressure', 'temperature']).groupby(['adsorbate', 'adsorbent']):
-        for q in np.linspace(0.15, 0.85, 15):
-            q = gr['adsorption'].quantile(q)
-            bl = ~(gr['adsorption'] > 1.05*q) & ~(gr['adsorption'] < 0.95*q)
-            if bl.sum() > 2:
-                invars.append((gr['temperature'].mean(), np.log(gr['pressure'].mean()), gr['invar'][bl].mean()))
-
-    t, p, i = zip(*invars)
-    ndf = pd.DataFrame(np.transpose([t, p, i]), columns=['temperature', 'log pressure', 'invar'])
-
-    def std_over_mean(x):
-        return x.std() / x.mean()
-    r = ndf.agg(['mean', 'std', std_over_mean])
-    return r
-
 if __name__ == '__main__':
+    from read_data import read_isotherm_data
     df = read_isotherm_data()
 
-    #r = test_invar(df)
-    #print(r)
-
     #plot_K_T(df)
-    plot_a_p(df)
-    plt.show()
-
-    ## this gives the exponents as the 'constants' (empirical model has two parameters, coefficient and exponent)
-    #ndf = fit_freundlichs(df)
-    #gr = ndf.groupby(['doi', 'adsorbent', 'adsorbate'])['n'].agg(['mean', 'std'])
-    #print(gr.dropna().sort_values(by=('std')))
+    #plot_a_p(df)
+    #plt.show()
