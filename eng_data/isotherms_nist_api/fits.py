047851e2d68f485f98abe831a6e56354878b43bb
david <davidollodart@gmail.com>
2021-11-14 14:07:06 -0800

    langmuir constant fits require 5 or more points
    actually 6 since one of them is used to normalize the data

--- eng_data/isotherms_nist_api/fits.py
+++ eng_data/isotherms_nist_api/fits.py
@@ -28,7 +28,7 @@ def fit_langmuir_explicit(p, Sigma):
         K = (g1*p0 - pi)/(g2*(1. - g1))
         bl = (~np.isnan(K)) & (~np.isinf(K) & (K>0)) # this should prevent bad data points from being included
         m = K[bl].mean()
-        if not np.isnan(m) and not np.isinf(m) and m > 0:
+        if not np.isnan(m) and not np.isinf(m) and m > 0 and len(bl) - bl.sum() > 5: # minimum 5 data points
             Klst.append(m)
 
     # may want to impose some maximum residual on the data to only return results which have meaningful fits
@@ -59,3 +59,16 @@ def fit_langmuirs(df):
 
 def fit_freundlichs(df):
     return _fit(df, fit_freundlich, 'n')
+
+if __name__ == '__main__':
+    from read_data import read_isotherm_data
+    df = read_isotherm_data()
+
+    ## this gives the exponents as the 'constants' (empirical model has two parameters, coefficient and exponent)
+    ndf = fit_freundlichs(df)
+    gr = ndf.groupby(['doi', 'adsorbent', 'adsorbate'])['n'].agg(['mean', 'std'])
+    print(gr.dropna().sort_values(by=('std')))
+
+    ndf = fit_langmuirs(df)
+    gr = ndf.groupby(['doi', 'adsorbent', 'adsorbate'])['K'].agg(['mean', 'std'])
+    print(gr.dropna().sort_values(by=('std')))
