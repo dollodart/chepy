ec272b701054ebf10099575ac7bbb15b109c7716
David Ollodart <davidollodart@gmail.com>
2021-12-11 14:43:52 -0800

    change caching of isotherms and material lookup
    now the filenames mirror the api strings used to get them

--- eng_data/isotherms_nist_api/store-local.py
+++ eng_data/isotherms_nist_api/store-local.py
@@ -1,3 +1,4 @@
+import pathlib
 import json
 import requests
 from time import time
@@ -14,40 +15,49 @@ def get_isotherms_abbreviated():
         write_file.write(json.dumps(isos))
 
 
-def get_isotherms(start=0, overwrite=False):
+
+def get_isotherms(max_queries=1e100, overwrite=False):
+
     try:
         with open(f'{OUT_DIR}/all_isos_abbreviated.json', 'r') as read_file:
             isos = json.loads(read_file.read())
     except FileNotFoundError:
         raise FileNotFoundError("Run get_isotherms_abbreviated before get_isotherms")
+    
+    out_dir = pathlib.Path(f'{OUT_DIR}')
+    cache = []
+    for f in out_dir.iterdir():
+        if f.name.startswith('isotherm-'):
+            cache.append(f.name.lstrip('isotherm-').rstrip('.json'))
 
-    for d, iso in enumerate(isos[start:]):
-        try:
-            with open(f'{OUT_DIR}/{c:04d}-data', 'r') as _:
-                print(f'already have data for {c:04d}')
-                if not overwrite:
-                    print('skipping this query')
-                    continue
-                else:
-                    print('overwriting this data')
-        except FileNotFoundError:
-            pass
-        c = start + d
+    for c, iso in enumerate(isos):
+        fname = iso['filename'].lower()
+
+        if fname in cache:
+            print(f'already have data for {fname} at position {c:04d} in all_isos_abbreviated.json')
+            if not overwrite:
+                print('skipping this query')
+                continue
+            else:
+                print('overwriting this data')
+        else:
+            print(f'no record for {fname} at position {c:04d} in all_isos_abbreviated.json')
+            print(f'querying for this data')
+
         t0 = time()
         hkey = iso['adsorbent']['hashkey']
-        fname = iso['filename']
 
         name = requests.get(f'{API_URL}/material/{hkey}.json').json()
-        with open('{OUT_DIR}/{0:04d}-name-query'.format(c), 'w') as write_file:
+        with open(f'{OUT_DIR}/material-{hkey}.json', 'w') as write_file:
             write_file.write(json.dumps(name))
 
         data = requests.get(f'{API_URL}/isotherm/{fname}.json').json()
 
-        with open(f'{OUT_DIR}/{c:04d}-data', 'w') as write_file:
+        with open(f'{OUT_DIR}/isotherm-{fname}.json', 'w') as write_file:
             write_file.write(json.dumps(data))
 
-        print(f'collected data for {c:04d} in {time()-t0:.1f}s')
+        print(f'collected data for {fname} at position {c:04d} in {time()-t0:.1f}s')
 
 if __name__ == '__main__':
-    get_isotherms_abbreviated()
+    #get_isotherms_abbreviated()
     get_isotherms(overwrite=False)
