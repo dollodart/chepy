cfd73a5ca506e40d8ebdda07c6d9e2265c3d9ae4
David Ollodart <davidollodart@gmail.com>
2021-12-11 16:48:41 -0800

    added pubchem api logic to prevent repeat null result query

--- eng_data/isotherms_nist_api/pubchem_api.py
+++ eng_data/isotherms_nist_api/pubchem_api.py
@@ -20,10 +20,15 @@ def mw_req(inchi):
 
     print(f'running query on {inchi}')
     record = url.get().json()
-    val = float(record['PropertyTable']['Properties'][0]['MolecularWeight']) 
-    global_dct[inchi] = val
+    try:
+        val = float(record['PropertyTable']['Properties'][0]['MolecularWeight']) 
+        global_dct[inchi] = val
+    except KeyError:
+        global_dct[inchi] = None
+
     return global_dct[inchi]
 
 if __name__ == '__main__':
-    mw = mw_req('YNQLUTRBYVCPMQ-UHFFFAOYSA-N')
+    #mw = mw_req('YNQLUTRBYVCPMQ-UHFFFAOYSA-N')
+    mw = mw_req('STZCRXQWRGQSJD-GEEYTBSJSA-M')
     print(mw)
